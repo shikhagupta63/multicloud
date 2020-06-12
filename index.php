ed953fb771404a17a83df36bf56c1da1845eb1bf
shikhagupta63 <66768096+shikhagupta63@users.noreply.github.com>
2020-06-12 14:16:20 +0530

    comment1

---
+++ index.php
@@ -0,0 +1,23 @@
+<html>
+<head>
+<title></title>
+<style>
+body{
+color:white;
+background-color:grey;
+}
+h2{
+color: red;
+}
+</style>
+</head>
+<body>
+<h2>hello terraform</h2>
+<?php
+
+
+print "ifconfig";
+
+?>
+</body>
+</html>
